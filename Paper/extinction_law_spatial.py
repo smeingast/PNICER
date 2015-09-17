f9996c2face465b849417b2528dded14b441eac8
Stefan Meingast <stefan.meingast@gmail.com>
2015-09-17 09:28:12 +0200

    Added extinction map plot and IRAC contours to spatial extinction law

--- Paper/extinction_law_spatial.py
+++ Paper/extinction_law_spatial.py
@@ -6,6 +6,7 @@ import brewer2mpl
 # import numpy as np
 import matplotlib.pyplot as plt
 
+from astropy.io import fits
 from itertools import repeat
 from multiprocessing import Pool
 from matplotlib.pyplot import GridSpec
@@ -16,6 +17,7 @@ from helper import *
 
 # ----------------------------------------------------------------------
 # Load colorbrewer colormap
+cmap0 = brewer2mpl.get_map("Greys", "Sequential", number=9, reverse=False).get_mpl_colormap(N=500, gamma=1)
 cmap1 = brewer2mpl.get_map("Spectral", "Diverging", number=11, reverse=True).get_mpl_colormap(N=10, gamma=1)
 cmap2 = brewer2mpl.get_map("YlGnBu", "Sequential", number=9, reverse=True).get_mpl_colormap(N=10, gamma=1)
 
@@ -28,14 +30,17 @@ science_color, control_color = science.mag2color(), control.mag2color()
 
 # ----------------------------------------------------------------------
 # Get grid
-header, all_glon, all_glat = science.build_wcs_grid(frame="galactic", pixsize=10 / 60)
+pixsize = 2 / 60
+header, all_glon, all_glat = science.build_wcs_grid(frame="galactic", pixsize=pixsize)
 grid_shape = all_glat.shape
 glon_range, glat_range = all_glon.ravel(), all_glat.ravel()
 
 
 # ----------------------------------------------------------------------
 # Initial guess for extinction
-ext = science_color.pnicer(control=control_color).extinction
+pnicer = science_color.pnicer(control=control_color)
+ext = pnicer.extinction
+emap = pnicer.build_map(bandwidth=pixsize * 2, metric="epanechnikov")
 
 
 # ----------------------------------------------------------------------
@@ -90,10 +95,10 @@ def get_slope(glon_pix, glat_pix, glon_all, glat_all, maxdis):
     xdata_control = control.features[1][cfil] - control.features[2][cfil]
     ydata_control = control.features[0][cfil] - control.features[1][cfil]
 
-    # # Get ordinary least squares slope
+    # Get ordinary least squares slope
     # beta_ols = get_beta_ols(xj=xdata_science, yj=ydata_science)
-    #
-    # # Get BCES slope
+
+    # Get BCES slope
     # beta_bces = get_beta_bces(x_sc=xdata_science, y_sc=ydata_science,
     #                           cov_err_sc=-np.mean(science.features_err[1][sfil]) ** 2,
     #                           var_err_sc=np.mean(science.features_err[1][sfil]) ** 2 +
@@ -113,28 +118,40 @@ def get_slope(glon_pix, glat_pix, glon_all, glat_all, maxdis):
     return beta_lines, n
 
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    with Pool() as pool:
-        mp = pool.starmap(get_slope, zip(glon_range, glat_range, repeat(science.lon), repeat(science.lat), repeat(0.3)))
 
-    # Unpack results
-    slope, nsources = list(zip(*mp))
+if __name__ == "__main__":
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        with Pool(4) as pool:
+            mp = pool.starmap(get_slope, zip(glon_range, glat_range, repeat(science.lon),
+                                             repeat(science.lat), repeat(0.2)))
+else:
+    mp = 0
+    exit()
+
+# Unpack results
+slope, nsources = list(zip(*mp))
 
 # Convert results to arrays
 slope, nsources = np.array(slope), np.array(nsources, dtype=float)
 
 # plt.imshow(np.array(slope).reshape(grid_shape), cmap="brg", vmin=2.45, vmax=2.55, interpolation="nearest")
-fig = plt.figure(figsize=[10, 6])
-grid = GridSpec(ncols=2, nrows=2, bottom=0.05, top=0.95, left=0.05, right=0.95, hspace=0.1, wspace=0.1,
+fig = plt.figure(figsize=[11, 10])
+grid = GridSpec(ncols=2, nrows=3, bottom=0.05, top=0.95, left=0.05, right=0.95, hspace=0.1, wspace=0.1,
                 width_ratios=[1, 0.02])
 # Add axes
-ax1 = plt.subplot(grid[0], projection=wcsaxes.WCS(header=header))
-cax1 = plt.subplot(grid[1])
-ax2 = plt.subplot(grid[2], projection=wcsaxes.WCS(header=header))
-cax2 = plt.subplot(grid[3])
+ax0 = plt.subplot(grid[0], projection=wcsaxes.WCS(header=emap.fits_header))
+cax0 = plt.subplot(grid[1])
+ax1 = plt.subplot(grid[2], projection=wcsaxes.WCS(header=header))
+cax1 = plt.subplot(grid[3])
+ax2 = plt.subplot(grid[4], projection=wcsaxes.WCS(header=header))
+cax2 = plt.subplot(grid[5])
 
-# Plot data
+# Plot extinction map
+im0 = ax0.imshow(emap.map, interpolation="nearest", origin="lower", vmin=0, vmax=2, cmap=cmap0)
+plt.colorbar(im0, cax=cax0, ticks=MultipleLocator(0.25), label="$A_K$")
+
+# Plot slope
 im1 = ax1.imshow(slope.reshape(grid_shape), cmap=cmap1, interpolation="nearest", origin="lower", vmin=1.68, vmax=1.78)
 plt.colorbar(im1, cax=cax1, ticks=MultipleLocator(0.02), label="Slope")
 
@@ -143,4 +160,17 @@ plt.colorbar(im1, cax=cax1, ticks=MultipleLocator(0.02), label="Slope")
 im2 = ax2.imshow(nsources.reshape(grid_shape), cmap=cmap2, origin="lower", interpolation="nearest")
 plt.colorbar(im2, cax=cax2, label="#")
 
-plt.show()
+# Draw IRAC1 coverage
+irac1_coverage_path = "/Users/Antares/Dropbox/Data/Orion/Other/Spitzer_Orion_IRAC1_coverage_s.fits"
+irac1_coverage = fits.open(irac1_coverage_path)[0].data
+irac1_coverage_header = fits.open(irac1_coverage_path)[0].header
+
+for ax in [ax0, ax1, ax2]:
+    ax.set_xlim(0, grid_shape[1])
+    ax.set_ylim(0, grid_shape[0])
+    ax.contour(irac1_coverage, levels=[0, 1], transform=ax.get_transform(wcsaxes.WCS(irac1_coverage_header)),
+               colors="black")
+
+# Save figure
+results_path = "/Users/Antares/Dropbox/Projects/PNICER/Paper/Results/"
+plt.savefig(results_path + "extinction_law_spatial.pdf", bbox_inches="tight", dpi=300)
