e04427910d957d718f9de95e69adb8b5bf440c42
Stefan Meingast <stefan.meingast@gmail.com>
2016-06-22 16:06:33 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.core import setup
 setup(
     name="PNICER",
     version="0.1",
-    packages=["pnicer"],
+    packages=["pnicer", "pnicer.tests", "pnicer.tests_resources"],
     package_dir={"pnicer": "pnicer"},
     package_data={"pnicer": ["tests_resources/*.fits"]},
+    data_files=[("notebooks", ["pnicer.ipynb"])],
     install_requires=["numpy", "scikit-learn", "matplotlib", "astropy", "wcsaxes"],
     url="",
     license="",
