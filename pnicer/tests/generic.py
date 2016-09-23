88d02d725b01524a1f1dddfa4d5d689ef2ecf076
Stefan Meingast <stefan.meingast@gmail.com>
2016-09-23 13:22:59 +0200

    Cleaned Orion test code

--- pnicer/tests/generic.py
+++ pnicer/tests/generic.py
@@ -1,6 +1,5 @@
 def orion():
 
-    # ----------------------------------------------------------------------
     # Import stuff
     from astropy.io import fits
     from astropy.coordinates import SkyCoord
@@ -8,20 +7,17 @@ def orion():
     from pnicer import Magnitudes
     from pnicer.utils import get_resource_path
 
-    """ This file goes through a typical PNICER session and creates a rough extinction map of Orion A from 2MASS data. """
+    """ This file goes through a typical PNICER session and creates an extinction map of Orion A from 2MASS data. """
 
-    # ----------------------------------------------------------------------
     # Find the test files
     science_path = get_resource_path(package="pnicer.tests_resources", resource="Orion_A_2mass.fits")
     control_path = get_resource_path(package="pnicer.tests_resources", resource="CF_2mass.fits")
 
-    # ----------------------------------------------------------------------
     # Define feature names and extinction vector
     feature_names = ["Jmag", "Hmag", "Kmag"]
     error_names = ["e_Jmag", "e_Hmag", "e_Kmag"]
     feature_extinction = [2.5, 1.55, 1.0]
 
-    # ----------------------------------------------------------------------
     # Load data
     with fits.open(science_path) as science, fits.open(control_path) as control:
         sci, con = science[1].data, control[1].data
@@ -36,24 +32,21 @@ def orion():
         # Errors
         sci_err, con_err = [sci[n] for n in error_names], [con[n] for n in error_names]
 
-    # ----------------------------------------------------------------------
     # Initialize pnicer
     science = Magnitudes(mag=sci_phot, err=sci_err, extvec=feature_extinction, coordinates=sci_coo, names=feature_names)
     control = Magnitudes(mag=con_phot, err=con_err, extvec=feature_extinction, coordinates=con_coo, names=feature_names)
 
-    # ----------------------------------------------------------------------
-    # Run extinction codes
+    # Test PNICER
     pnicer = science.pnicer(control=control, add_colors=False)
-    nicer = science.nicer(control=control)
 
-    # ----------------------------------------------------------------------
-    # Test making extinction maps
+    # Test NICER
+    science.nicer(control=control)
+
+    # Make extinction map
     pnicer_emap = pnicer.build_map(bandwidth=5 / 60, metric="gaussian", nicest=False, use_fwhm=False)
-    nicer_emap = pnicer.build_map(bandwidth=5 / 60, metric="gaussian", sampling=2, nicest=False, use_fwhm=False)
 
-    # ----------------------------------------------------------------------
     # Plot the PNICER extinction map
     pnicer_emap.plot_map(figsize=10)
 
     """ If no errors pop up, then the basic PNICER package works """
-    print("{0:<80s}".format("PNICER routines terminated successfully! Happy extinciton mapping :) "))
+    print("{0:<80s}".format("PNICER routines terminated successfully! Happy extinction mapping :) "))
