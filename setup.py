1c78ca099ed47b53397251a0d16d7187df6f31cf
Stefan Meingast <stefan.meingast@gmail.com>
2023-11-30 00:05:31 +0100

    Updated requirements

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
-import sys
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-# Require Python 3
-if sys.version_info < (3, 4):
-    sys.exit("Sorry, Python < 3.4 is not supported")
-
+# Grab the long description from the README
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="PNICER",
-    version="0.1",
-    packages=["pnicer", "pnicer.tests", "pnicer.tests_resources", "pnicer.utils"],
+    version="0.3",
+    packages=find_packages(),
     package_dir={"pnicer": "pnicer"},
     package_data={"pnicer": ["tests_resources/*.fits"]},
-    install_requires=["numpy>=1.11", "scipy>=0.18", "scikit-learn>=0.18", "matplotlib>=1.5", "astropy>=1.3"],
-    url="",
-    license="",
+    python_requires=">=3.11",
+    install_requires=[
+        "numpy>=1.26.2",
+        "scipy>=1.11.4",
+        "scikit-learn>=1.3.2",
+        "matplotlib>=3.8.2",
+        "astropy>=6.0.0",
+    ],
+    url="https://github.com/smeingast/PNICER",
     author="Stefan Meingast",
     author_email="stefan.meingast@gmail.com",
-    description="Python package to calculate color-excesses and extinction from arbitrary photometric measurements."
+    description="Python package to calculate color-excesses and "
+                "extinction from arbitrary photometric measurements.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
