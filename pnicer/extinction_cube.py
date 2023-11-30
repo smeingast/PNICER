95bbdd20e7ebcd97e8cc564308e5a4f1f48b822a
Stefan Meingast <stefan.meingast@gmail.com>
2023-11-30 09:20:49 +0100

    Reformatted with black

--- pnicer/extinction_cube.py
+++ pnicer/extinction_cube.py
@@ -6,28 +6,26 @@ from astropy.io import fits
 from pnicer.utils.algebra import round_partial
 
 
-# ----------------------------------------------------------------------------- #
-# ----------------------------------------------------------------------------- #
 class ExtinctionCube:
-    # -----------------------------------------------------------------------------
     def __init__(self, cube, header=None):
         self.cube = cube
         self.header = header if header is not None else fits.Header(self.cube)
 
         # Get third axis range
         if self.header is not None:
-            self.crange3 = (np.arange(self.cube.shape[0]) * self.header["CDELT3"] +
-                            self.header["CRVAL3"] - (self.header["CRPIX3"] - 1) * self.header["CDELT3"])
+            self.crange3 = (
+                np.arange(self.cube.shape[0]) * self.header["CDELT3"]
+                + self.header["CRVAL3"]
+                - (self.header["CRPIX3"] - 1) * self.header["CDELT3"]
+            )
         else:
             self.crange3 = np.arange(self.cube.shape[0])
 
-    # -----------------------------------------------------------------------------
     @property
     def shape(self):
         return self.cube.shape
 
-    # -----------------------------------------------------------------------------
-    def save_fits(self, path, overwrite=True, precision=1E-4):
+    def save_fits(self, path, overwrite=True, precision=1e-4):
         """
         Save extinction cube as FITS file.
 
@@ -42,5 +40,7 @@ class ExtinctionCube:
 
         """
 
-        hdu = fits.PrimaryHDU(round_partial(self.cube, precision=precision), header=self.header)
+        hdu = fits.PrimaryHDU(
+            round_partial(self.cube, precision=precision), header=self.header
+        )
         hdu.writeto(path, overwrite=overwrite)
